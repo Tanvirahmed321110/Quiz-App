be5285194bdc4a0dfb9f7bcba6ad03d07d574018
tanvir321110 <tanvirahmed321110@gmail.com>
2023-09-10 03:06:26 +0600

    show all question options

--- app.js
+++ app.js
@@ -1,3 +1,64 @@
+const questionData = [
+    {
+        id : 1,
+        question : ' What Dose HTML Stand For ?',
+        anwser : 'hyper text markup language',
+        option : [
+            'hyper text markup language',
+            'hyper text  language ',
+            'Hoper text markup style ',
+            'hyper text markup language'
+        ]
+    },
+    {
+        id : 2,
+        question : 'What is mean of css .... ?',
+        anwser : 'case cading style sheet',
+        option : [
+            'case text markup language',
+            'hyper text  language ',
+            'Hoper text markup style ',
+            'hyper text markup language'
+        ]
+    },
+    {
+        id : 3,
+        question : 'How many zilla in Bangladesh ?',
+        anwser : 'hyper text markup language',
+        option : [
+            '64 text markup language',
+            'hyper text  language ',
+            'Hoper text markup style ',
+            'hyper text markup language'
+        ]
+    },
+    {
+        id : 4,
+        question : 'How many village in bangladesh?',
+        anwser : 'hyper text markup language',
+        option : [
+            'hyper text markup language',
+            'hyper text  language ',
+            'Hoper text markup style ',
+            'hyper text markup language'
+        ]
+    },
+    {
+        id : 5,
+        question : 'what is chat cpt  ?',
+        anwser : 'hyper text markup language',
+        option : [
+            'hyper text markup language',
+            'hyper text  language ',
+            'Hoper text markup style ',
+            'hyper text markup language'
+        ]
+    },
+]
+
+
+
+
 
 let startQuizBtn = document.querySelector('.btn-container button');
 let exitBtn = document.querySelector('.btn-exit');
@@ -27,12 +88,11 @@ exitBtn.addEventListener('click',()=>{
 })
 
 
-
 // btn continue 
 continueBtn.onclick = function(){
     questionPage.style.display='block';
     rulesPage.style.display='none';
-    showQuestion()
+    showQuestion(count)
 }
 
 // pre btn
@@ -43,11 +103,33 @@ preBtn.addEventListener('click',()=>{
 
 
 
+let count = 0;
+// next btn
+document.querySelector('.btn-next').addEventListener('click',function(){
+    if(count < questionData.length){
+        count++;
+        showQuestion(count)
+    }
+    else{
+        console.log("Findsh ")
+    }
+})
 
 
 // question 
-const showQuestion = ()=>{
+const showQuestion = (index)=>{
     let questions = document.querySelector('.question-tittle');
-    let questionText =  `<h3> ${questionData[2].question} </h3>`
+    let questionText =  `<h3> ${questionData[index].id} .  ${questionData[index].question} </h3>`
     questions.innerHTML = questionText;
+    
+    let optionsContainer = document.querySelector(".options-container");
+    optionsContainer.innerHTML  = `
+        <div class="option"> <p>${questionData[index].option[0]} </p> </div> 
+        <div class="option"> <p>${questionData[index].option[1]} </p>  </div> 
+        <div class="option"><p>${questionData[index].option[2]} </p> </div> 
+        <div class="option"><p>${questionData[index].option[3]} </p> </div> 
+    `
+    
+    let pageNumber = document.querySelector('.footer-left-container');
+    pageNumber.innerHTML = `<div class="footer-left">${index+1} of 5 question </div>`
 }
--- question-data.js
+++ question-data.js
@@ -1,57 +1,57 @@
-const questionData = [
-    {
-        id : 1,
-        question : ' What Dose HTML Stand For ?',
-        anwser : 'hyper text markup language',
-        option : [
-            'hyper text markup language',
-            'hyper text  language ',
-            'Hoper text markup style ',
-            'hyper text markup language'
-        ]
-    },
-    {
-        id : 2,
-        question : 'What is mean of css .... ?',
-        anwser : 'case cading style sheet',
-        option : [
-            'hyper text markup language',
-            'hyper text  language ',
-            'Hoper text markup style ',
-            'hyper text markup language'
-        ]
-    },
-    {
-        id : 3,
-        question : 'How many zilla in Bangladesh ?',
-        anwser : 'hyper text markup language',
-        option : [
-            'hyper text markup language',
-            'hyper text  language ',
-            'Hoper text markup style ',
-            'hyper text markup language'
-        ]
-    },
-    {
-        id : 4,
-        question : 'How many village in bangladesh?',
-        anwser : 'hyper text markup language',
-        option : [
-            'hyper text markup language',
-            'hyper text  language ',
-            'Hoper text markup style ',
-            'hyper text markup language'
-        ]
-    },
-    {
-        id : 5,
-        question : 'what is chat cpt  ?',
-        anwser : 'hyper text markup language',
-        option : [
-            'hyper text markup language',
-            'hyper text  language ',
-            'Hoper text markup style ',
-            'hyper text markup language'
-        ]
-    },
-]
+// const questionData = [
+//     {
+//         id : 1,
+//         question : ' What Dose HTML Stand For ?',
+//         anwser : 'hyper text markup language',
+//         option : [
+//             'hyper text markup language',
+//             'hyper text  language ',
+//             'Hoper text markup style ',
+//             'hyper text markup language'
+//         ]
+//     },
+//     {
+//         id : 2,
+//         question : 'What is mean of css .... ?',
+//         anwser : 'case cading style sheet',
+//         option : [
+//             'hyper text markup language',
+//             'hyper text  language ',
+//             'Hoper text markup style ',
+//             'hyper text markup language'
+//         ]
+//     },
+//     {
+//         id : 3,
+//         question : 'How many zilla in Bangladesh ?',
+//         anwser : 'hyper text markup language',
+//         option : [
+//             'hyper text markup language',
+//             'hyper text  language ',
+//             'Hoper text markup style ',
+//             'hyper text markup language'
+//         ]
+//     },
+//     {
+//         id : 4,
+//         question : 'How many village in bangladesh?',
+//         anwser : 'hyper text markup language',
+//         option : [
+//             'hyper text markup language',
+//             'hyper text  language ',
+//             'Hoper text markup style ',
+//             'hyper text markup language'
+//         ]
+//     },
+//     {
+//         id : 5,
+//         question : 'what is chat cpt  ?',
+//         anwser : 'hyper text markup language',
+//         option : [
+//             'hyper text markup language',
+//             'hyper text  language ',
+//             'Hoper text markup style ',
+//             'hyper text markup language'
+//         ]
+//     },
+// ]
